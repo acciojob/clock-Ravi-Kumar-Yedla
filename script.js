a82525e5d8f08aa6777af0b813f38df60ebfdf8c
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-02-27 10:02:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,63 +1,76 @@
 //your JS code here. If required.
-let para = document.querySelector("#timer")
+// let para = document.querySelector("#timer")
 
 
-function currentTime() {
-	  let date = new Date()
-	  let hour = date.getHours()
-	  let min = date.getMinutes()
-	  let sec = date.getSeconds()
-	  let day = date.getDay()  
-      let month = date.getMonth()
-      let year = date.getFullYear()
-	   let arrayofTime = convertTime(hour, min, sec) 
-       // [hour, min, sec, AM/PM]
-       for(i = 0; i<=2; i++){
-        arrayofTime[i] = addZero(arrayofTime[i])
-       }
-    let time = `${arrayofTime[0]}:${arrayofTime[1]}:${arrayofTime[2]} ${arrayofTime[3]}`
-      para.innerHTML = `${month}/${day}/${year}, ${time} `
+// function currentTime() {
+// 	  let date = new Date()
+// 	  let hour = date.getHours()
+// 	  let min = date.getMinutes()
+// 	  let sec = date.getSeconds()
+// 	  let day = date.getDay()  
+//       let month = date.getMonth()
+//       let year = date.getFullYear()
+// 	   let arrayofTime = convertTime(hour, min, sec) 
+//        // [hour, min, sec, AM/PM]
+//        for(i = 0; i<=2; i++){
+//         arrayofTime[i] = addZero(arrayofTime[i])
+//        }
+//     let time = `${arrayofTime[0]}:${arrayofTime[1]}:${arrayofTime[2]} ${arrayofTime[3]}`
+//       para.innerHTML = `${month}/${day}/${year}, ${time} `
 
-	// return `${month}/${day}/${year}, ${time} `
-}
+// 	// return `${month}/${day}/${year}, ${time} `
+// }
 
-// let ans = currentTime()
-// alert(ans)
+// // let ans = currentTime()
+// // alert(ans)
 
-setInterval(currentTime,1000)
+// setInterval(currentTime,1000)
 
 
 
-function convertTime(hr, min, sec){
+// function convertTime(hr, min, sec){
 
-  //  11:59Pm = 23:59
-  // 12:00Am = 00:00
+//   //  11:59Pm = 23:59
+//   // 12:00Am = 00:00
 
-    if(hr>12){
-      hr = hr - 12  
-    return [hr, min, sec, "PM"]
-    }
-    else if(hr == 0){
-        // return `12:${min}:${sec} AM`
-        return [12, min, sec, "AM"]
-    }
-    else if(hr<12){
-    //   return `${hr}:${min}:${sec} AM`
-      return [hr, min, sec, "AM"]
-    }
-    else if(hr == 12){
-        // return `${hr}:${min}:${sec} PM`
-        return [hr, min, sec, "PM"]
-    }
+//     if(hr>12){
+//       hr = hr - 12  
+//     return [hr, min, sec, "PM"]
+//     }
+//     else if(hr == 0){
+//         // return `12:${min}:${sec} AM`
+//         return [12, min, sec, "AM"]
+//     }
+//     else if(hr<12){
+//     //   return `${hr}:${min}:${sec} AM`
+//       return [hr, min, sec, "AM"]
+//     }
+//     else if(hr == 12){
+//         // return `${hr}:${min}:${sec} PM`
+//         return [hr, min, sec, "PM"]
+//     }
     
-}
+// }
 
 
-function addZero(k){
-  if(k<10){
-    return "0"+k
-  }
-  else {
-    return k
-  }
+// function addZero(k){
+//   if(k<10){
+//     return "0"+k
+//   }
+//   else {
+//     return k
+//   }
+// }
+
+
+
+
+
+
+function updateTimer() {
+  const now = new Date();
+  const timerElement = document.getElementById('timer');
+  timerElement.textContent = now.toLocaleString();
 }
+
+setInterval(updateTimer, 1000);
